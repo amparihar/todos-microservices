44479dd05ae11c7df02343313403016842ab4239
EC2 Default User <ec2-user@ip-172-31-17-202.us-east-2.compute.internal>
2023-09-27 08:30:18 +0000

    updated frontend taskdef.json

--- ECS-Terraform/modules/deployment/s3.tf
+++ ECS-Terraform/modules/deployment/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "artifacts" {
   bucket = "deployment-artifacts-${local.name_suffix}-${random_string.random.result}"
   acl    = "private"
-
+  force_destroy = true
   lifecycle_rule {
     id      = "clean-up"
     enabled = "true"
